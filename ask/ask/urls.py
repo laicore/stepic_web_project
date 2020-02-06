2599834526328e5030421186768589a88b3c601d
laicore <kamil.talipov97@gmail.com>
2020-02-06 17:52:16 +0300

    123

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .wsgi import fun
 
 urlpatterns = [
+    path('', fun),
     path('admin/', admin.site.urls),
+    path('login/', fun),
+    path('signup/', fun),
+    path('question/', include('qa.urls')),
+    path('ask/', fun),
+    path('popular/', fun),
+    path('new/', fun)
+
 ]
--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,9 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+from django.http import HttpResponse
+
+
+def fun(request):
+    return HttpResponse('1OK\n')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ask.settings')
 
