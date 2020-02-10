7f86c2308869b62c3ed940fb31008a70d43838f0
laicore <kamil.talipov97@gmail.com>
2020-02-10 17:28:51 +0300

    123

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mysql.connector
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ''
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file':'my.cnf',
+        },
     }
 }
 
--- init.sh
+++ init.sh
@@ -1,16 +1,18 @@
+#sudo /etc/init.d/mysql start
+#mysql -uroot -e "create database testdb"
+#mysql -uroot -e "create user 'user'@'localhost' identified by 'user'"
+#mysql -uroot -e "grant all on testdb.* to 'user'@'localhost'"
 echo "unlink default server nginx"
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo apt update
-sudo apt install python3.5
+sudo apt install python3.5 libmysqlclient-dev
 sudo unlink /usr/bin/python3 
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip gunicorn
 sudo python3 -m pip install django==2.1
-gunicorn -b 0.0.0.0:8000 --chdir ~/web/ask ask.wsgi &
-echo "gunicorn start django"
-sudo /etc/init.d/nginx start
+#gunicorn -b 0.0.0.0:8000 --chdir ~/web/ask ask.wsgi &
+#sudo /etc/init.d/nginx start
 #sudo nginx
-echo "nginx start"
-echo "finish"
+#echo "finish"
 
